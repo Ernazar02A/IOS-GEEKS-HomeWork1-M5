c5fa68326b33873c5c5f93a868700b0acd47b8dc
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-07-10 15:37:43 +0600

    create class custom View

--- IOS-GEEKS-HomeWork1-M5/View/CustomView.swift
+++ IOS-GEEKS-HomeWork1-M5/View/CustomView.swift
@@ -5,4 +5,27 @@
 //  Created by Ernazar on 10/7/23.
 //
 
-import Foundation
+import UIKit
+
+class CustomView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        layoutViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+        layoutViews()
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.systemBackground
+    }
+    
+    func layoutViews() {
+        
+    }
+}
